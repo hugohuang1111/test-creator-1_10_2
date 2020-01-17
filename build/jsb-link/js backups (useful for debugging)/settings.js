469f728e0b8fc0f7eaf0d5ebda1291f31c01c490
hugo <hugohuang1111@gmail.com>
2020-01-17 11:34:04 +0800

    add export project

---
+++ build/jsb-link/js backups (useful for debugging)/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"ios",groupList:["default"],collisionMatrix:[[true]],rawAssets:{assets:{}},assetTypes:[],launchScene:"db://assets/main.fire",scenes:[{url:"db://assets/main.fire",uuid:"55GS9K1R9Me6BSKiAcJncS"}],packedAssets:{},orientation:"",subpackages:{},uuids:[]};
